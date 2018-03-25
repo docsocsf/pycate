5d370dafbdf04cdf6e06da219f7c2befa35dfe3a
Fraser May <frasertmay@gmail.com>
2018-03-25 14:31:59 +0100

    Converted Exercise model to use @property

--- pycate/models.py
+++ pycate/models.py
@@ -2,16 +2,16 @@ class Exercise:
     def __init__(
             self, module_number, module_name, code, name, start, end,
             assessed_status, submission_status, links, spec_key):
-        self.module_number = module_number
-        self.module_name = module_name
-        self.code = code
-        self.name = name
-        self.start = start
-        self.end = end
-        self.assessed_status = assessed_status
-        self.submission_status = submission_status
-        self.links = links
-        self.spec_key = spec_key
+        self.__module_number = module_number
+        self.__module_name = module_name
+        self.__code = code
+        self.__name = name
+        self.__start = start
+        self.__end = end
+        self.__assessed_status = assessed_status
+        self.__submission_status = submission_status
+        self.__links = links
+        self.__spec_key = spec_key
 
     def __str__(self):
         return 'Exercise{{Module={} {};Code={};Name={}}}'.format(
@@ -20,3 +20,43 @@ class Exercise:
             self.code,
             self.name
         )
+
+    @property
+    def module_number(self):
+        return self.__module_number
+
+    @property
+    def module_name(self):
+        return self.__module_name
+
+    @property
+    def code(self):
+        return self.__code
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def start(self):
+        return self.__start
+
+    @property
+    def end(self):
+        return self.__end
+
+    @property
+    def assessed_status(self):
+        return self.__assessed_status
+
+    @property
+    def submission_status(self):
+        return self.__submission_status
+
+    @property
+    def links(self):
+        return self.__links
+
+    @property
+    def spec_key(self):
+        return self.__spec_key
