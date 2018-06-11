d5a093754d27802f853617e3744cd0c47d10f43a
Fraser May <frasertmay@gmail.com>
2018-06-12 00:32:56 +0100

    Reformatted setup.py with black

--- setup.py
+++ setup.py
@@ -3,46 +3,38 @@ from setuptools import setup, find_packages
 import re
 from os import path
 
-PACKAGE_NAME = 'pycate'
+PACKAGE_NAME = "pycate"
 HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(HERE, "README.rst"), encoding="utf-8") as f:
     README = f.read()
 
-# Get version number from pycate/const.py
-with open(path.join(HERE, PACKAGE_NAME, 'const.py'), encoding='utf-8') as fp:
-    VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
+# Get version number from pycate/const.py, must be surrounded by "
+with open(path.join(HERE, PACKAGE_NAME, "const.py"), encoding="utf-8") as fp:
+    VERSION = re.search("__version__ = \"([^\"]+)\"", fp.read()).group(1)
 
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
-
-    description='An API wrapper for CATe',
+    description="An API wrapper for CATe",
     long_description=README,
-    url='https://github.com/docsocsf/py-cate',
-
-    author='Fraser May',
-    author_email='frasertmay@gmail.com',
-
-    license='MIT',
-
+    url="https://github.com/docsocsf/py-cate",
+    author="Fraser May",
+    author_email="frasertmay@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Environment :: Console'
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent'
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Utilities'
+        "Development Status :: 1 - Planning",
+        "Environment :: Console" "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent" "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
     ],
-    keywords='cate api imperial college',
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    install_requires=['requests', 'beautifulsoup4', 'html5lib'],
-    tests_require=['pytest'],
-    python_requires='~=3.6'
+    keywords="cate api imperial college",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    install_requires=["requests", "beautifulsoup4", "html5lib"],
+    tests_require=["pytest"],
+    python_requires="~=3.6",
 )
